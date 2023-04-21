24667d3917012297a48dd8f84f2c47c9e1aaa4c4
amitgupta <aksamit1625@gmail.com>
2023-04-22 03:31:50 +0530

    fir commit

---
+++ src/App.js
@@ -0,0 +1,56 @@
+import React from "react";
+import Home from "./pages/Home";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import Cart from "./pages/Cart";
+import Product from "./components/Product";
+
+import {
+  createBrowserRouter,
+  Outlet,
+  RouterProvider,
+  ScrollRestoration,
+} from "react-router-dom";
+import { productsData } from "./api/Api";
+
+const Layout = () => {
+  return (
+    <div>
+      <Header />
+      <Outlet />
+      <Footer />
+    </div>
+  );
+};
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Layout />,
+    children: [
+      {
+         path:"/",
+         element: <Home/>,
+         loader: productsData,
+      },
+      {
+        path : "/product/:id",
+        element : <Product/>
+      },
+      {
+        path: "/cart",
+        element : <Cart/>
+      },
+    ],
+  },
+]);
+
+function App() {
+  return (
+    <div className="font-bodyFont">
+         <RouterProvider router={router}/>
+    </div>
+  );
+}
+
+export default App;
