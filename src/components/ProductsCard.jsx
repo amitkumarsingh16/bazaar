b86766ee33626e99eff2e5cb16e384aa4bf9d121
amitgupta <aksamit1625@gmail.com>
2023-04-25 04:14:34 +0530

    update

--- src/components/ProductsCard.jsx
+++ src/components/ProductsCard.jsx
@@ -3,6 +3,7 @@ import {BsArrowRight} from "react-icons/bs"
 import { useDispatch } from "react-redux";
 import { useNavigate } from "react-router-dom";
 import { addToCart } from "../redux/bazaarSlice";
+import { ToastContainer, toast } from 'react-toastify';
 
 const ProductsCard = ({ product }) => {
 
@@ -45,16 +46,14 @@ const ProductsCard = ({ product }) => {
             <p className="font-semibold">${product.price}</p>
             </div>
             <p onClick={()=>dispatch(addToCart({
-
               _id: product._id,
               title: product.title,
               image: product.image,
               price: product.price,
               quantity: 1,
               description: product.description,
-
-            })) 
-            } className="absolute z-20 w-[100px] text-gray-500 hover:text-gray-900 flex items-center gap-1 top-0 transform -translate-x-32 group-hover:translate-x-0 transition-transform cursor-pointer duration-500">add to cart <span><BsArrowRight/></span>
+            }))
+            & toast.success(`${product.title} is added`)} className="absolute z-20 w-[100px] text-gray-500 hover:text-gray-900 flex items-center gap-1 top-0 transform -translate-x-32 group-hover:translate-x-0 transition-transform cursor-pointer duration-500">add to cart <span><BsArrowRight/></span>
             </p>
           </div>
         </div>
@@ -63,8 +62,21 @@ const ProductsCard = ({ product }) => {
         </div>
         <div className="absolute top-4 right-0">{product.isNew && (
           <p className="bg-black text-white font-semibold font-titleFont px-6 py-1">Sale</p>
-        )}</div>
+        )}
+        </div>
       </div>
+      <ToastContainer
+        position="top-left"
+        autoClose={2000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+        theme="dark"
+      />
     </div>
   );
 };
